99ffc0976f3e32784f8664cc4beb7155e8d2f46c
Crowdbotics <team@crowdbotics.com>
2021-08-17 03:19:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gunfire_29869/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gunfire_29869;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gunfire_29869";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "gunfire_29869Identifier",
+  url: "https://gunfire_29869Identifier.botics.co",
+  api: "https://gunfire_29869Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
